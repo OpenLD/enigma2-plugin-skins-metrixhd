490d28ab95a61d5f1216a139a74a7aca83a0875d
Javier Sayago <admin@lonasdigital.com>
2018-04-23 02:35:28 +0200

    update files

--- usr/lib/enigma2/python/Components/Converter/MetrixSTBinfo.py
+++ usr/lib/enigma2/python/Components/Converter/MetrixSTBinfo.py
@@ -1,14 +1,10 @@
-from Components.About import about
 from Components.Converter.Converter import Converter
 from Components.config import config
 from Components.Element import cached
-from time import *
-from types import *
-from sys import modules
-import sys, os, time, socket, fcntl, struct, subprocess, threading, traceback, commands, datetime
-from os import path, system, remove as os_remove, rename as os_rename, popen, getcwd, chdir
+from os import path, popen
 from Plugins.Extensions.MyMetrixLite.__init__ import initOtherConfig
 import Screens.Standby
+from time import time
 
 initOtherConfig()
 
@@ -54,7 +50,7 @@ class MetrixSTBinfo(Converter, object):
 		except:
 			pass
 		etime = time()
-		info += space + "Time: " + str(int(float(etime - stime)*3000)) + " ms"
+		info += space + "Time: " + str(int(float(etime - stime)*6000)) + " ms"
 		return info
 
 	def getCPUload(self):
@@ -66,12 +62,6 @@ class MetrixSTBinfo(Converter, object):
 			#info = "CPU-Load: " + temp
 			info = temp.replace('\n', '').replace(' ','')
 			info = _("CPU-Load: %s") % info
-		return info
-
-	def getRAMfree(self):
-		info = ""
-		info = str(about.getRAMFreeString())
-		info = _("RAM-Free: %s MB") % info
 		return info
 
 	def getCPUtemp(self):
@@ -106,6 +96,24 @@ class MetrixSTBinfo(Converter, object):
 			#info ="SYS-Temp: " + temp.replace('\n', '') + str('\xc2\xb0') + "C"
 			info = temp.replace('\n', '').replace(' ','') + str('\xc2\xb0') + "C"
 			info = _("SYS-Temp: %s") % info
+		return info
+
+	def getRAMfree(self):
+		info = ""
+		if path.exists('/proc/meminfo'):
+			f = open('/proc/meminfo', 'r')
+			temp = f.readlines()
+			f.close()
+			try:
+				for lines in temp:
+					lisp = lines.split()
+					if lisp[0] == "MemFree:":
+						#info = "RAM-Free: " + str(int(lisp[1]) / 1024) + " MB"
+						info = str(int(lisp[1]) / 1024)
+						info = _("RAM-Free: %s MB") % info
+						break
+			except:
+				pass
 		return info
 
 	def getFLASHfree(self):
